745e64a39beaa7c6cd9972f0d37bbe0bfaa0e316
iscra-sama <nyaascorka@gmail.com>
2026-03-22 14:50:57 +0000

    upd

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const express = require("express");
 
 const app = express();
 app.get(/.*/, (req, res) => {
-    res.end("okay");
+    res.end("iscrookay");
 });
+app.listen(process.env.PORT || 8000);
